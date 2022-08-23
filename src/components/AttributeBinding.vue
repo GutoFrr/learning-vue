d3c89c777758ce4ffd60022d3cfb625c9d508aee
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-08-23 18:40:21 -0300

    movedattribute binding to own component

---
+++ src/components/AttributeBinding.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const isButtonDisabled = [false, true]
+</script>
+
+<template>
+  <div class="card btn-container">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <button :disabled="isButtonDisabled[0]" type="button" @click="count = 0">
+      reset count
+    </button>
+    <button :disabled="isButtonDisabled[1]" type="button" @click="count = 100">
+      set count 100
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.btn-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-
+import AttributeBinding from './AttributeBinding.vue'
 defineProps({
   msg: String,
 })
-
-const count = ref(0)
-const isResetDisabled = false
-const isSetCountHundredDisabled = true
-const isButtonDisabled = [false, true]
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <button :disabled="isButtonDisabled[0]" type="button" @click="count = 0">
-      reset count
-    </button>
-    <button :disabled="isButtonDisabled[1]" type="button" @click="count = 100">
-      set count 100
-    </button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
+  <AttributeBinding />
   <p class="read-the-docs">Click on the Vue logo to learn more</p>
 </template>
 
