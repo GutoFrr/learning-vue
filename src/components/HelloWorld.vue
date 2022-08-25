3f29e72d3d65d13ccf0db095e70bc0e2a748714f
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-08-25 17:21:44 -0300

    single component - props

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <script setup>
 import AttributeBindings from './AttributeBindings.vue'
 import ConditionalsAndLoops from './ConditionalsAndLoops.vue'
-import FormBindings from './FormBindings.vue';
+import FormBindings from './FormBindings.vue'
+import SingleComponent from './SingleComponent.vue'
 
 defineProps({
   msg: String,
@@ -17,6 +18,8 @@ defineProps({
   <ConditionalsAndLoops title="Conditionals and Loops" />
   <hr />
   <FormBindings title="Form Bindings" />
+  <hr />
+  <SingleComponent title="Single Component" />
   <p class="read-the-docs">Click on the Vue logo to learn more</p>
 </template>
 
---
+++ src/components/SingleComponent.vue
@@ -0,0 +1,32 @@
+<!--
+Here we show the simplest possible component which accepts a prop and renders it.
+Learn more about components in the guide!
+-->
+
+<script setup>
+import { ref } from 'vue'
+import TodoItem from './TodoItem.vue'
+
+defineProps({
+  title: String,
+})
+
+const groceryList = ref([
+  { id: 0, text: 'Vegetables' },
+  { id: 1, text: 'Cheese' },
+  { id: 2, text: 'Dog Shit' },
+])
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+  <ol>
+    <!--
+      We are providing each todo-item with the todo object
+      it's representing, so that its content can be dynamic.
+      We also need to provide each component with a "key",
+      which is explained in the guide section on v-for.
+    -->
+    <TodoItem v-for="item in groceryList" :todo="item" :key="item.id" />
+  </ol>
+</template>
